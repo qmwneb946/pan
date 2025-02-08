b663661cf840cdb408280ac28fa5c3907714fa35
qmwneb946 <qmwneb946@gmail.com>
2025-02-08 10:29:52 +0800

    Add files via upload

---
+++ files/CF/template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using i64 = long long;
+
+void solve () {
+	
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	assert(freopen("in.txt", "r", stdin));
+	assert(freopen("out.txt", "w", stdout));
+#endif
+	int t = 1;
+	std::cin >> t;
+	while (t--) {
+		solve();
+	}
+}
